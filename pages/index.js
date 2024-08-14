27f1e9dd51ef243aa32134a2b101419a0061e2e1
24LP8078 24LP8078 <24LP8078@G305-CC0038.local>
2024-08-14 11:25:48 +0800

    zasaw

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import Box from "./components/box";
-import Round from "./components/round";
-import Input from "./components2/Input";
 import Background from "./components2/Background";
-import Button from "./components2/Button";
+
 export default function Home() {
   return (
     <div>
